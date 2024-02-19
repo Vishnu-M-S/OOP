e79c5e572ae891eaff6e4e0c1f19239567889244
Vishnu-M-S <63758397+Vishnu-M-S@users.noreply.github.com>
2024-02-19 10:18:39 +0000

    Add files via upload

---
+++ Matrix_Add.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class Matrix
+{
+int a[][]=new int[10][10];
+Scanner sc=new Scanner(System.in);
+void create(int l)
+{
+	for(int i=0;i<l;i++)
+		for(int j=0;j<l;j++)
+		a[i][j]=sc.nextInt();
+}
+void display(int l)
+{
+
+for(int i=0;i<l;i++)
+	{
+		for(int j=0;j<l;j++)
+		System.out.print(a[i][j]+" ");
+		System.out.println("");
+	}
+}
+}
+class Matrix_Add
+{
+public static void main(String args[])
+{
+Matrix m1,m2,sum;
+int l;
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter size of matrix: ");
+l=sc.nextInt();
+m1=new Matrix();
+m2=new Matrix();
+sum=new Matrix();
+System.out.println("Enter the elements of first matrix: ");
+m1.create(l);
+System.out.println("Enter the elements of second matrix: ");
+m2.create(l);
+
+for(int i=0;i<l;i++)
+	{
+		for(int j=0;j<l;j++)
+		sum.a[i][j]=m1.a[i][j]+m2.a[i][j];
+	}		
+
+System.out.println("The sum mstrix is:");
+sum.display(l);	
+}
+}
+
