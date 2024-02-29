87765226d7bc04dcc212d5ac8b4f9a07cf0d4ec4
Vishnu-M-S <63758397+Vishnu-M-S@users.noreply.github.com>
2024-02-29 09:34:11 +0000

    Update ComplexNumber.java

--- ComplexNumber.java
+++ ComplexNumber.java
@@ -1,35 +1,40 @@
-class ComplexNumber {
-	int real, image;
-	public ComplexNumber(int r, int i)
-	{
-		this.real = r;
-		this.image = i;
-	}
+import java.util.Scanner;
 
-	public void showC()
+class complex
+{
+	int r, i;
+	complex(int r, int i)
 	{
-		System.out.println(this.real + " +i" + this.image);
-	}
-
-	public static ComplexNumber add(ComplexNumber n1,ComplexNumber n2)
-	{
-		ComplexNumber res = new ComplexNumber(0, 0);
-		res.real = n1.real + n2.real;
-		res.image = n1.image + n2.image;
-		return res;
-	}
-
-	public static void main(String arg[])
-	{
-		ComplexNumber c1 = new ComplexNumber(4, 5);
-		ComplexNumber c2 = new ComplexNumber(10, 5);
-		System.out.print("first Complex number: ");
-		c1.showC();		
-		System.out.print("\nSecond Complex number: ");
-		c2.showC();
-		ComplexNumber res = add(c1, c2);
-		System.out.print("\nAddition is :");
-		res.showC();
+		this.r = r;
+		this.i = i;
 	}
 }
 
+class ComplexNumber
+{
+	public static void main(String args[])
+	{
+			
+		Scanner sc = new Scanner(System.in);
+		System.out.println("First number:");
+		System.out.print("Enter real part: ");
+		int r = sc.nextInt();
+		System.out.print("Enter image : ");
+		int i = sc.nextInt();
+		
+		complex c1 = new complex(r, i);
+		
+		System.out.println("Second number:");
+		System.out.print("Enter real part : ");
+		r = sc.nextInt();
+		System.out.print("Enter the image : ");
+		i = sc.nextInt();
+		
+		complex c2 = new complex(r, i);
+		
+		int real = c1.r + c2.r;
+		int image = c1.i + c2.i;
+		
+		System.out.println("Complex number : " + real + " + " + image +"i");
+	}
+}
